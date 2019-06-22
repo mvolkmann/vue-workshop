19794a06e9aeb3ef327ed6c5b5cb250f0f4d1ee3
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-06-21 20:12:57 -0500

    removed an eslint-disable

--- todo/stories/index.stories.js
+++ todo/stories/index.stories.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/react-in-jsx-scope */
-
 import {storiesOf} from '@storybook/vue';
 import {action} from '@storybook/addon-actions';
 //import {linkTo} from '@storybook/addon-links';
